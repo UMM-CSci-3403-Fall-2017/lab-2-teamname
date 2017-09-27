cbfed8607e2be87032a3ffe6737f6533f46fab74
twarling <warli010@morris.umn.edu>
2017-09-27 18:44:09 -0500

    Added functionality to disemvowel.c by returning a new null-terminated string that has all vowels removed from it. All tests pass.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,26 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "disemvowel.h"
 
 char* disemvowel(char* str) {
-  return (char*) "";
+        int len;
+        char* result;
+        int i;
+        int num = 1;
+        int resultLocation = 0;
+
+        len = strlen(str);
+        result = (char*) calloc(num, sizeof(char));
+ 	
+	for (i = 0; i < len; ++i){
+		if(!(str[i] == 'a'||str[i] == 'e'|| str[i] == 'i'|| str[i] == 'o'|| str[i] == 'u'|| str[i] == 'O'|| str[i] == 'A'|| str[i] == 'E'|| str[i] == 'I'|| str[i] == 'U')) {
+			result[resultLocation] = str[i];
+ 			num++;
+			resultLocation++;
+		
+                }
+        }       
+        return result;
 }
