8b7fdea4d918b1e265393d84de76204cb4509f0c
twarling <warli010@morris.umn.edu>
2017-09-28 11:19:39 -0500

    Fixed allocation issue that was causing errors. Also cleaned up code and added explanatory comments

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -8,19 +8,28 @@ char* disemvowel(char* str) {
         int len;
         char* result;
         int i;
-        int num = 1;
         int resultLocation = 0;
+	int numVowels = 0;
 
         len = strlen(str);
-        result = (char*) calloc(num, sizeof(char));
- 	
-	for (i = 0; i < len; ++i){
+	
+	//counts number of vowels in the string
+	for(i = 0; i < len; i++){
+		if((str[i] == 'a'||str[i] == 'e'|| str[i] == 'i'|| str[i] == 'o'|| str[i] == 'u'|| str[i] == 'O'|| str[i] == 'A'|| str[i] == 'E'|| str[i] == 'I'|| str[i] == 'U')){
+			numVowels++;
+		}
+	}
+
+
+	//allocates memory to result that takes into account that string will be smaller since it has no vowels
+        result = (char*) calloc(len - numVowels + 1, sizeof(char));
+
+	//checks each character to see if it is a vowel. If it isn't a vowel, the character is copied into result.
+	for (i = 0; i < len; i++){
 		if(!(str[i] == 'a'||str[i] == 'e'|| str[i] == 'i'|| str[i] == 'o'|| str[i] == 'u'|| str[i] == 'O'|| str[i] == 'A'|| str[i] == 'E'|| str[i] == 'I'|| str[i] == 'U')) {
 			result[resultLocation] = str[i];
- 			num++;
 			resultLocation++;
-		
                 }
-        }       
+        } 	
         return result;
 }
