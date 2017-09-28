0fdaa04ef41572a23ae42d58117c323c7d07d17a
twarling <warli010@morris.umn.edu>
2017-09-28 10:29:51 -0500

    Fixed the memory leak, all tests still pass.

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -5,12 +5,16 @@
 
 int main(int argc, char* argv[]) {
   char* line;
+  char* holder;
   size_t size;
-  
+
   size = 100;
   line = (char*) malloc (size + 1);
 
   while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+    holder = disemvowel(line);
+    printf("%s\n", holder);
+    free(holder);
   }
+  free(line);
 }
